953f8a615d7219dd9eb1052651419b75f3cd6623
ehingley19 <hingesme@gmail.com>
2024-12-27 15:37:02 +0000

    Introduce templates.

--- ApplicationTemplate/app/routes.py
+++ ApplicationTemplate/app/routes.py
@@ -1,10 +1,11 @@
+from flask import render_template
 from app import app
 
 # These lines cause an empty URL and the /index URL to be mapped to the index() view function
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Default page'
+    return render_template('index.html', title='Default Page', header='Main Header')
 
 # This line causes the /help URL to be mapped to the help() view function
 @app.route('/help')
